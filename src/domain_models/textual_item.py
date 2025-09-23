d5d15a1d8f9d70ce88ae2e9994293447f8484df2
Austin <alamar@DESKTOP-D2VNCOM.localdomain>
2025-09-22 23:00:50 -0400

    fetch book by title fetches many matches

--- src/domain_models/textual_item.py
+++ src/domain_models/textual_item.py
@@ -1,10 +1,25 @@
+import requests
+url = "https://openlibrary.org/search.json?q="
+
 class TextualItem:
     def __init__(self, title: str, isbn: str):
-        data = TextualItem._fetch_metadata(title, isbn)
+        self.data = TextualItem._fetch_metadata(title, isbn)
         self.title = title
-        self.isbn = isbn
+        self.isbn = isbn 
 
-    def _fetch_metadata(title: str ="", isbn: str=""):
+    @classmethod
+    def _fetch_metadata(cls, title: str ="", isbn: str=""):
         """Fetch metadata from an external source."""
         if not title and not isbn:
             raise ValueError
+        if title:
+            title = title.replace(" ", "+")
+        query = isbn if isbn else title
+        response = requests.get(url + query)
+        if response.status_code != 200:
+            raise ConnectionError("Failed to fetch metadata")
+        data = response.json()
+        if data['numFound'] == 0:
+            raise ValueError("No metadata found")
+
+        return data 
--- tests/domain_models/test_textual_item.py
+++ tests/domain_models/test_textual_item.py
@@ -2,9 +2,17 @@ import pytest
 
 from src.domain_models.textual_item import TextualItem
 
-def test__fetch_metadata():
+def test_fetch_metadata_no_values():
     title = ""
     isbn = ""
 
     with pytest.raises(ValueError):
         TextualItem(title, isbn)
+
+def test_fetch_metadata_with_title():
+    title = "The Great Gatsby"
+    isbn = ""  
+    query = TextualItem(title, isbn)
+
+    assert query.data['numFound'] == 991
+    assert query.data['docs'][0]['title'] == "The Great Gatsby"
